597cd1087098d67a43237a5757c4e6d3c485b452
Angela Diaz <angeldi846@gmail.com>
2022-12-01 00:45:15 -0500

    Componente footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import Footer from './Footer';
 
 export default function App() {
   return (
+    <>
      <h1>hola mundo</h1>
+     <Footer/>
+    </> 
   )
 }
 
