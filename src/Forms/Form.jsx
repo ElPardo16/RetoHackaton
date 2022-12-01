478bb245dddc3a50e791cc4aaeae045cb6bfa9db
Amanda Forero <amandaforero@outlook.com>
2022-12-01 01:22:04 -0500

    Creación de carpeta para componentes de form, estilos

---
+++ src/Forms/Form.jsx
@@ -0,0 +1,21 @@
+import formLogo from "../img/Logo1.png";
+import "./Form.css";
+
+function Form({ children }) {
+  return (
+    <section className="form-Log-In">
+      <img src={formLogo} alt="" />
+      <div className="container">
+        <div className="buttons">
+          <button className="log-in-button">Iniciar sesión</button>
+          <button className="sign-up-button">Registrarse</button>
+        </div>
+        <div className="form">
+          {children}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Form;
