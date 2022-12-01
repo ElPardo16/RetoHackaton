f61b268c7a18d8ecacaa1cf883c53ca7e0656020
ElPardo16 <andresito105@gmail.com>
2022-11-30 22:58:21 -0500

    objeto bussines

---
+++ src/bussines.js
@@ -0,0 +1,15 @@
+export const bussines = [{
+  name: "Desarrollador FullStack",
+  des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  img: "./img/laccenture.jpeg"  
+},
+{
+    name: "Desarrollador backend",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "./img/lglobant.jpg"  
+},
+{
+    name: "Desarollador SR",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    img: "./img/lpercient.jpg"  
+}]
